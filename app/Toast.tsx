f5fce6fdaf24ec7806a84fb00aff004c30d19e42
lilKriT <lilKriTwebDev@gmail.com>
2026-02-25 20:50:58 +0100

    Lots of work.

---
+++ app/Toast.tsx
@@ -0,0 +1,9 @@
+const Toast = () => {
+  return (
+    <div className="absolute top-16 left-1/2 -translate-x-1/2  bg-pink-300">
+      Message Sent! Thanks for completing the form. We'll be in touch soon!
+    </div>
+  );
+};
+
+export default Toast;
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,57 @@
-import Image from "next/image";
+import Toast from "./Toast";
 
 export default function Home() {
   return (
-    <div className="">
-      <p>Contact Us</p>
+    <section className="min-h-dvh flex justify-center items-center">
+      <div className="max-w-4xl bg-white flex flex-wrap gap-y-4 gap-x-4 px-12 py-10 rounded-3xl">
+        <h1 className="text-[clamp(var(--text-3xl),8vw,(var(--text-4xl)))] text-gray-900 basis-full">
+          Contact Us
+        </h1>
 
-      <p>First Name This field is required</p>
+        <label className="field field--half">
+          First Name
+          <input type="text" name="" id="" />
+          <span className="error">This field is required</span>
+        </label>
 
-      <p>Last Name This field is required</p>
+        <label className="field field--half">
+          Last Name
+          <input type="text" />
+          <span className="error">This field is required</span>
+        </label>
 
-      <p>
-        Email Address Please enter a valid email address This field is required
-      </p>
+        <label className="field field--full">
+          Email Address
+          <input type="text" />
+          <span className="error">
+            Please enter a valid email address This field is required
+          </span>
+        </label>
 
-      <p>
-        Query Type General Enquiry Support Request Please select a query type
-      </p>
+        <p>
+          Query Type General Enquiry Support Request Please select a query type
+        </p>
 
-      <p>Message This field is required</p>
+        {/* Message */}
+        <label className="field field--full">
+          <p>Message</p>
+          <textarea name="" id="" rows={4} className=""></textarea>
+          <span className="error">This field is required</span>
+        </label>
 
-      <p>
-        I consent to being contacted by the team To submit this form, please
-        consent to being contacted
-      </p>
+        {/* Consent checkbox */}
+        <label className="field field--full field--horizontal flex-wrap">
+          <input type="checkbox" name="" id="" className="basis-8 shrink-0" />
+          <p className="">I consent to being contacted by the team</p>
+          <span className="error basis-full">
+            To submit this form, please consent to being contacted
+          </span>
+        </label>
 
-      <p>Submit</p>
+        <button className="btn btn--primary basis-full">Submit</button>
 
-      <p>
-        Message Sent! Thanks for completing the form. We'll be in touch soon!
-      </p>
-    </div>
+        <Toast />
+      </div>
+    </section>
   );
 }
